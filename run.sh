310ab71353b4e4e8c22f955b5c104472f84ec128
cesardlq <cdlq13@gmail.com>
2018-09-04 10:13:37 -0500

    Automatic run added

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Running RDCMST on giraph..."
+cd my-app
+echo "Compiling..."
+mvn package
+echo "Removing output directory..."
+rm -r ../exampleOut
+echo "Executing..."
+giraph target/my-app-1.0-SNAPSHOT.jar edu.icesi.app.EdgeRemovalComputation  \
+-vif edu.icesi.app.RDCMSTVertexInputFormat \
+-vip input/exampleRDCMST.txt \
+-eof org.apache.giraph.io.formats.SrcIdDstIdEdgeValueTextOutputFormat \
+-op ../exampleOut \
+-w 1 \
+-mc edu.icesi.app.RDCMSTMasterCompute \
+-ca giraph.SplitMasterWorker=false \
+-ca giraph.logLevel=ERROR
+
